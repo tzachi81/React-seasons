2111732ce3286802a8dfa0348ba8b691da1796ad
tzachi81 <tzahih@gmail.com>
2019-09-15 01:26:49 +0300

    use state anf if statement for error handling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        
+        this.state = {lat: null, errorMessage: ''};
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.error(err)
-    );
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({lat: position.coords.latitude})
+            },
+            err => {
+                this.setState({errorMessage: err.message});
+            }
+        );
+    }
 
-    return <div>Hi!</div>
-}
+    render(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat}</div>;
+        }
 
+        return <div>Loading...</div>;
+    }
+}    
 ReactDOM.render(
     <App/>,
     document.querySelector('#root')

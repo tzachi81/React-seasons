cc93304722320521df015c4a88f631aea072e56d
tzachi81 <tzahih@gmail.com>
2019-09-15 23:44:34 +0300

    conditional seasonDisplay with icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-
-const SeasonDisplay = () => {
-    return <div>Season display</div>
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9){
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'summer'? 'Let\'s hit the beach' : 'Brr. it is chilli';
+    // <i class="sun icon"></i>
+    // <i class="snowflake icon"></i>
+    const icon = season === 'summer'? 'sun': 'snowflake';
+    return <div>
+        <i className={`${icon} icon`}></i>
+        <h1>{text}</h1>
+        <i className={`${icon} icon`}></i>
+        </div>
 }
 
 export default SeasonDisplay;

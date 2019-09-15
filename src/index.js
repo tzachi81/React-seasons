76c2fdc74a40f5c4c598183b5adc9a39a2b842bd
tzachi81 <tzahih@gmail.com>
2019-09-15 23:43:28 +0300

    use state as props and refactor code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        
-        this.state = {lat: null, errorMessage: ''};
 
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat: position.coords.latitude})
-            },
-            err => {
-                this.setState({errorMessage: err.message});
-            }
-        );
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
+        )
     }
 
     render(){
@@ -22,12 +18,13 @@ class App extends React.Component{
             return <div>Error: {this.state.errorMessage}</div>
         }
         if (!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
         return <div>Loading...</div>;
     }
 }    
+
 ReactDOM.render(
     <App/>,
     document.querySelector('#root')
